1ad9dd091d135931bba4f35aa8682b19945b760c
Sarah Paz <sarah.paz@gmail.com>
2018-12-22 05:23:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(number)
-  first_number = (1...-1)
-  second_number = (1...-1)
+  first_number = (1...100)
+  second_number = (1...100)
 
   if number == first_number * second_number
     false
